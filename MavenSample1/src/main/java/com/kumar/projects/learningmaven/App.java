fd4f08dd9a603a5003cf49c9a2b3e2f0f9950791
Saravana Kumar M <kumar9885035454@gmail.com>
2016-07-01 09:53:12 +0530

    This is the initial commit. Just copied all the maven projects. Also updated the .gitignore file.

---
+++ MavenSample1/src/main/java/com/kumar/projects/learningmaven/App.java
@@ -0,0 +1,13 @@
+package com.kumar.projects.learningmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
